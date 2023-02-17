ac2830baedcd66b1e2de273e4e56179ac7e332c2
Sandeep kumar <sandeep.b@codebrewinnovation.com>
2023-02-17 15:07:51 +0530

    create account upto trustline

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./home.css"
 import { Field, Form, Formik} from "formik"
 import { Button } from 'react-bootstrap'
+import axios from 'axios'
+import StellarSdk, { TransactionBuilder, BASE_FEE, Operation, Asset, Keypair } from "stellar-sdk";
+
+// const StellarSdk = require('stellar-sdk');
+const server = new StellarSdk.Server("https://horizon-testnet.stellar.org/")
+
 
 export const Home = () => {
+
+const[accountAddress,setAccountAddress] = useState("")
+const[accountBalance,setAccountBalance] = useState(0)
+const[anchorAddress,setAnchorAddress] = useState("")
+  
+  
+async function createAccount(){
+  const pair1 = StellarSdk.Keypair.random();
+  const _Address= pair1.publicKey()
+  setAccountAddress(_Address)
+  console.log(_Address,"Destination Public key (destination address)");
+  const privatekey = pair1.secret();
+  console.log(privatekey," Destination Secret key");
+  localStorage.setItem("PrivateKey",privatekey);    //storing privateKey to loacl storage ********************************
+  const response = await axios.get(`https://friendbot.stellar.org/?addr=${_Address}`)
+    console.log(response,"response>>>>>>>>>>>>>>>>>>>")
+  await CheckBalance(_Address)
+}
+   
+async function CheckBalance(accountAddress){
+  const CheckBalance = await axios.get(`https://horizon-testnet.stellar.org/accounts/${accountAddress}`)
+    console.log(CheckBalance,".....................................for payment....");  
+    setAccountBalance(CheckBalance?.data?.balances[0]?.balance)
+}
+async function BuildTrustLIne(){
+  const secret= localStorage.getItem("PrivateKey");
+  const keypair = Keypair.fromSecret(secret);
+  const networkUrl = "https://horizon-testnet.stellar.org/";
+  const assetCode = "USDC";
+  const server = new StellarSdk.Server(networkUrl);
+  const issuingAccount = anchorAddress; // Circle Anchor testnet Address :"GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5"
+  
+
+  const account = await server.loadAccount(accountAddress);
+
+  const transaction = new TransactionBuilder(account,{
+      fee: BASE_FEE,
+      networkPassphrase: StellarSdk.Networks.TESTNET,
+  }).addOperation(
+      Operation.changeTrust({
+          asset: new Asset(
+              assetCode,
+              issuingAccount
+          ),
+          limit: "100000",
+      }),
+  )
+  .setTimeout(0)
+  .build();
+
+  await transaction.sign(keypair);
+
+  const result = await server.submitTransaction(transaction);
+  console.log(result);
+  console.log("hurrrraaayyyyy");
+
+
+  return result;
+}
     return (
         <div className='main page-content'>
 
@@ -20,15 +85,15 @@ export const Home = () => {
                       <div className='row'>
                       <div className="col-md-12">
                       <div className='mb-3 gap-10'>
-                      <Button>
-                            Get Balance
+                      <Button onClick={CheckBalance}>
+                            Balance
                         </Button>
                         <label htmlFor="create">
                        <Field
                                 disabled={true}
                             //   name="userName"
                             //   id="username"
-                            value = "10"
+                            value = {accountBalance}
 
                               placeholder="XLM"
                               className="form-control"
@@ -37,7 +102,7 @@ export const Home = () => {
                        </label>
                       </div>
                       <div className='mb-3'>
-                      <Button>
+                      <Button onClick={BuildTrustLIne}>
                            Build TrustLine
                         </Button>
                         <label htmlFor="create">
@@ -46,16 +111,19 @@ export const Home = () => {
                             //   disabled={true}
                             //   name="userName"
                             //   id="username"
-                            //    value = "1123444"
+                               value = {anchorAddress}
                               placeholder="Anchor Address"
                               className="form-control"
+                              onChange={(e)=>setAnchorAddress(e.target.value)}
+                              
                             />
 
                        </label>
                       </div>
-                      <Button>
+                      <Button onClick={createAccount}>
                            Create Account
                         </Button>
+                        <div>{accountAddress}</div>
 
                      
                        
